836293e3180a30b2eb31bdbeec2eb1d2bc41fc29
autumn <109412646+autumn-2-net@users.noreply.github.com>
2024-06-15 17:58:50 +0800

    p1

---
+++ trainCLS/FVFBLCLS.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+from Models.CVNT import CVNT
+from Models.model_ada import MixModel
+
+from trainCLS.baseCLS import BasicCLS
+
+
+class FBLCLS(BasicCLS):
+    def __init__(self, config):
+        super().__init__(config)
+        self.model=CVNT(config,output_size=1)
+        self.loss=nn.BCEWithLogitsLoss()
+
+
+
+        self.gn = 0
+
+
+
+
+    def forward(self, x, mask=None):
+        return self.model(x, mask=mask)
+
+
+
+
+
