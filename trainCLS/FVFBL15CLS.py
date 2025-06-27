4aac5247c7a2a5da85c21753b2f885d4d40c4eb7
autumn <109412646+autumn-2-net@users.noreply.github.com>
2025-06-27 14:58:49 +0800

    add fbl 1.5

---
+++ trainCLS/FVFBL15CLS.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+from Models.CVNT import CVNT
+
+from trainCLS.baseCLS import BasicCLS
+
+
+class FBLCLS(BasicCLS):
+    def __init__(self, config):
+        super().__init__(config)
+        self.model = CVNT(config, output_size=4)
+        self.loss = nn.BCEWithLogitsLoss()
+        self.gn = 0
+
+    def forward(self, x, mask=None):
+        return self.model(x, mask=mask)
